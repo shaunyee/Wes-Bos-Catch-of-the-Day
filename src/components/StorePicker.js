0ea6f8e2699445448bace79ae38b95f86501f97a
Shaun Yee <yeezy@Shauns-MBP.hsd1.ca.comcast.net>
2018-06-18 19:49:28 -0700

    adds store picker form and apply css to index.js file.

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,8 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 
 class StorePicker extends Component {
   render() {
-    return <p>I am the Store Picker!</p>
+    return (
+      <form className="store-selector">
+        <h2>Please Enter A Store</h2>
+        <input type="text" required placeholder="Store Name"/>
+        <button type="submit">Visit Store -></button>
+      </form>
+    );
   }
 }
  export default StorePicker;
