2fd8cc204b8d747d28f8c4f613441f0387eeafc1
Shaun Yee <yeezy@Shauns-MacBook-Pro.local>
2018-06-19 18:37:38 -0700

    Changes Headr component into stateless component and adds tagline prop

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
 import StorePicker from './components/StorePicker';
 import App from './components/App';
