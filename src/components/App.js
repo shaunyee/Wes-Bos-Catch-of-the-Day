77f26aae85239ba528b5e9912faf8dd693902393
Shaun Yee <yeezy@Shauns-MacBook-Pro.local>
2018-06-24 22:16:27 -0700

    creates fish component that houses all information being displayed on the app component

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import Header from './Header';
 import Inventory from './Inventory';
 import Order from './Order';
 import sampleFishes from '../sample-fishes';
+import Fish from './Fish';
 
 class App extends Component {
   state = {
@@ -24,6 +25,12 @@ class App extends Component {
           <Header
             tagline="Fresh Seafood Market"
             />
+            <ul className="fishes">
+              {Object.keys(this.state.fishes).map(key => <Fish 
+                key={key} 
+                details={this.state.fishes[key]} />
+              )}
+            </ul>
         </div>
         <Order />
         <Inventory 
