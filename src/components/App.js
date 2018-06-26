0010552a925ed239157834b873315716a00e5b74
Shaun Yee <yeezy@Shauns-MacBook-Pro.local>
2018-06-25 20:33:20 -0700

    creates functionality for updating the order state through buttons on app.js

--- src/components/App.js
+++ src/components/App.js
@@ -10,14 +10,23 @@ class App extends Component {
     fishes: {},
     order: {}
   };
+  
   addFish = (fish) => {
     const fishes = {...this.state.fishes};
     fishes[`fish${Date.now()}`] = fish;
     this.setState({ fishes });
   };
+  
   loadSampleFishes = () => {
     this.setState({fishes: sampleFishes})
-  }
+  };
+  
+  addToOrder = (key) => {
+    const order = {...this.state.order};
+    order[key] = order[key] + 1 || 1;
+    this.setState({ order });
+  };
+  
   render() {
     return(
       <div className="catch-of-the-day">
@@ -27,8 +36,11 @@ class App extends Component {
             />
             <ul className="fishes">
               {Object.keys(this.state.fishes).map(key => <Fish 
-                key={key} 
-                details={this.state.fishes[key]} />
+                key={key}
+                index={key} 
+                details={this.state.fishes[key]} 
+                addToOrder = {this.addToOrder}
+                />
               )}
             </ul>
         </div>
